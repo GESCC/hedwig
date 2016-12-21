350c862ea4b988c5ee076b1209f81a108e6609b6
geine <bonos2@wisdo.me>
2016-12-22 01:35:38 +0900

    Project Init

---
+++ hedwig/src/main/java/com/gescc/hedwig/HedwigApplication.java
@@ -0,0 +1,12 @@
+package com.gescc.hedwig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HedwigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HedwigApplication.class, args);
+	}
+}
