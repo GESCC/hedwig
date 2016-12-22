8119a54863e9d041ab0130b74629785c00756b96
geine <bonos2@wisdo.me>
2016-12-22 14:02:23 +0900

    Add UserService Interface

--- hedwig/src/main/java/com/gescc/hedwig/service/UserService.java
+++ hedwig/src/main/java/com/gescc/hedwig/service/UserService.java
@@ -1,5 +1,20 @@
 package com.gescc.hedwig.service;
 
+import com.gescc.hedwig.view.ResultView;
+import com.gescc.hedwig.vo.User;
+
+/**
+* UserService Interface
+* 
+* @author geine
+* @date 2016.12.22
+* @version a
+*/
 public interface UserService {
 
+	public ResultView createUser(User user);
+	public ResultView deleteUser(String userEmail);
+	public ResultView doLogin(User user);
+	public ResultView updateUser(User user);
+	
 }
