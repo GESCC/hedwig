64c646c0758cdfdae758716a95d721c312b9c5d9
geine <bonos2@wisdo.me>
2016-12-22 11:15:17 +0900

    Add VO Package

---
+++ hedwig/src/main/java/com/gescc/hedwig/vo/User.java
@@ -0,0 +1,33 @@
+package com.gescc.hedwig.vo;
+
+public class User {
+
+	private String email;
+	private String password;
+	private String phoneNumber;
+	
+	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	
+	@Override
+	public String toString() {
+		return "User [email=" + email + ", password=" + password + ", phoneNumber=" + phoneNumber + "]";
+	}
+}
