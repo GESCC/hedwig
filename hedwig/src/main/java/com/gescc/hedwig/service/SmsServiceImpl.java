2566a5f0c74ba86fc7ced10b4f0c58d2a9774121
Heebeck Lee <ChickenPaella@github.com>
2016-12-25 20:20:23 +0900

    Add KeyUtil

--- hedwig/src/main/java/com/gescc/hedwig/service/SmsServiceImpl.java
+++ hedwig/src/main/java/com/gescc/hedwig/service/SmsServiceImpl.java
@@ -1,7 +1,5 @@
 package com.gescc.hedwig.service;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 import com.gescc.hedwig.vo.Sms;
 
