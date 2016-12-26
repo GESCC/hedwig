cac2aa08ab4b1d454476704aff7fa59f89b8e01a
geine <bonos2@wisdo.me>
2016-12-26 16:22:48 +0900

    Add CallBack Request

--- hedwig/src/main/java/com/gescc/hedwig/service/SmsService.java
+++ hedwig/src/main/java/com/gescc/hedwig/service/SmsService.java
@@ -4,9 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.gescc.hedwig.vo.Sms;
+import com.mashape.unirest.http.exceptions.UnirestException;
 
 
 public interface SmsService {
 	
-	public void sendSMS(Sms sms);
+	public void sendSMS(Sms sms) throws UnirestException;
+	
+	public void sendCallBack(Sms sms, Object result) throws UnirestException;
 }
