fc194ec2b2a110c93d1571b0aaf7649ecd856a8f
Heebeck Lee <ChickenPaella@github.com>
2016-12-28 03:14:59 +0900

    Update SmsServiceImpl

--- hedwig/src/main/java/com/gescc/hedwig/service/SmsServiceImpl.java
+++ hedwig/src/main/java/com/gescc/hedwig/service/SmsServiceImpl.java
@@ -2,12 +2,19 @@ package com.gescc.hedwig.service;
 
 
 import com.gescc.hedwig.vo.Sms;
+import com.mashape.unirest.http.exceptions.UnirestException;
 
 public class SmsServiceImpl implements SmsService {
 	
 	@Override
 	public void sendSMS(Sms sms) {
 		System.out.println(sms.toString());
+	}
+
+	@Override
+	public void sendCallBack(Sms sms, Object result) throws UnirestException {
+		// TODO Auto-generated method stub
+		
 	}
 	
 }
