61c3fe3089eb2058073a5be4de208808693cfdde
DESKTOP-OCQLTU1\dh110 <dh179@naver.com>
2016-12-30 12:21:51 +0900

    Filter check IP

--- hedwig/src/main/java/com/gescc/hedwig/config/FilterConfig.java
+++ hedwig/src/main/java/com/gescc/hedwig/config/FilterConfig.java
@@ -1,10 +1,13 @@
 package com.gescc.hedwig.config;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import com.gescc.hedwig.interceptor.RegAppInterceptor;
+import com.gescc.hedwig.filter.AppFilter;
 
 @Configuration
 public class FilterConfig {
@@ -12,7 +15,10 @@ public class FilterConfig {
 	@Bean
 	public FilterRegistrationBean getFilterRegistrationBean()
 	{
-		FilterRegistrationBean registrationBean = new FilterRegistrationBean(new RegAppInterceptor());
+		
+		FilterRegistrationBean registrationBean = new FilterRegistrationBean(new AppFilter());
+		registrationBean.addUrlPatterns("/*");
+		
 		return registrationBean;
 	}
 }
