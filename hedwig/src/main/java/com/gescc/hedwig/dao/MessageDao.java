1161ad9aa75cdf760c821293c73f071a86a28942
geine <bonos2@wisdo.me>
2016-12-30 11:02:20 +0900

    Add SingleMessage Manage

---
+++ hedwig/src/main/java/com/gescc/hedwig/dao/MessageDao.java
@@ -0,0 +1,24 @@
+package com.gescc.hedwig.dao;
+
+import java.util.Date;
+import java.util.List;
+
+import com.gescc.hedwig.vo.Message;
+
+/**
+ * @author geine
+ *
+ */
+public interface MessageDao {
+
+	List<Message> getMessageListByDate(Date startDate, Date endDate);
+	
+	List<Message> getMessageListByApplicationName(String applicationName);
+	
+	List<Message> getMessageListAll();
+	
+	Boolean saveMessage(Message message);
+	
+	Boolean deleteLastMessage();
+	
+}
